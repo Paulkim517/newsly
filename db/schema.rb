e90b430fecb4204af55366dad49b49f6be025207
Julfi Chumber <julfichumber@Julfis-MacBook-Pro.local>
2015-08-14 16:40:23 -0700

    added nav bar and sign up/login tabs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150814230231) do
+ActiveRecord::Schema.define(version: 20150814231420) do
+
+  create_table "news", force: :cascade do |t|
+    t.string   "title"
+    t.string   "body"
+    t.string   "link_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
