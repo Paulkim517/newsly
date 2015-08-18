87d70452a0dac891e8e33b5e9f1a8b4facf4bef4
Joshua Baker <joshualbaker111@gmail.com>
2015-08-18 11:13:28 -0700

    ..

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,13 +1,13 @@
+
 require "httparty"
-
 class SiteController < ApplicationController
-	include HTTParty
-
-	def index
-   @response = HTTParty.get('http://api.nytimes.com/svc/topstories/v1/national.json?api-key=b7d44ad17cc1da6bd24cdfc172b20a81:5:72707211')
-	 @title =response.parsed_response['results'][1]['title']
-	 @body =response.parsed_response['results'][1]['abstract']
-   render :index
-  end
-
+include HTTParty
+ def index
+  	@response = HTTParty.get('http://api.nytimes.com/svc/topstories/v1/national.json?api-key=b7d44ad17cc1da6bd24cdfc172b20a81:5:72707211')
+	# body = JSON.parse(response)
+	 @title =response.parsed_response['results'][0]['title']
+	 @body =response.parsed_response['results'][0]['abstract']
+    
+  	render :index
+ end
 end
