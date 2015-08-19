f19765e3692bd9b4d26524000be49aaa271e20a2
Joshua Baker <joshualbaker111@gmail.com>
2015-08-19 13:24:47 -0700

    styling

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -3,7 +3,7 @@ $(function(){
   L.mapbox.accessToken = 'pk.eyJ1Ijoic2FoYXNoMTgyIiwiYSI6IjJhNGNjYzNiMTRmMGQ5MjkwYjcwMmEwY2IwYjkxMDk5In0.9d5E9GnR78TIUvQnjUrGxw';
   var map = L.mapbox.map('map', 'sahash182.n6o0oh90', {
             center: [34, -97],
-            zoom: 2,
+            zoom: 3,
             minZoom: 2,
             zoomControl: false,
             maxBounds: [[-90, -180], [90, 180]]
