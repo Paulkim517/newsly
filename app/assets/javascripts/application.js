7c3589ae2b2da1a7f0190210cc2f350f48da990e
Joshua Baker <joshualbaker111@gmail.com>
2015-08-20 14:51:30 -0700

    About page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,12 +16,78 @@
 //= require_tree .
 
 $(function(){
-	
-$(window).scroll(function(){
-   if($(window).scrollTop()<20){
-         $('.fader').stop(true,true).fadeIn("slow");
-   } else {
-         $('.fader').stop(true,true).fadeOut("slow");
-   }
+
+
+	var ticking = false;
+	var isFirefox = (/Firefox/i.test(navigator.userAgent));
+	var isIe = (/MSIE/i.test(navigator.userAgent)) || (/Trident.*rv\:11\./i.test(navigator.userAgent));
+	var scrollSensitivitySetting = 30; //Increase/decrease this number to change sensitivity to trackpad gestures (up = less sensitive; down = more sensitive) 
+	var slideDurationSetting = 800; //Amount of time for which slide is "locked"
+	var currentSlideNumber = 0;
+	var totalSlideNumber = $(".background").length;
+
+	// ------------- DETERMINE DELTA/SCROLL DIRECTION ------------- //
+	function parallaxScroll(evt) {
+	  if (isFirefox) {
+	    //Set delta for Firefox
+	    delta = evt.detail * (-120);
+	  } else if (isIe) {
+	    //Set delta for IE
+	    delta = -evt.deltaY;
+	  } else {
+	    //Set delta for all other browsers
+	    delta = evt.wheelDelta;
+	  }
+
+	  if (ticking != true) {
+	    if (delta <= -scrollSensitivitySetting) {
+	      //Down scroll
+	      ticking = true;
+	      if (currentSlideNumber !== totalSlideNumber - 1) {
+	        currentSlideNumber++;
+	        nextItem();
+	      }
+	      slideDurationTimeout(slideDurationSetting);
+	    }
+	    if (delta >= scrollSensitivitySetting) {
+	      //Up scroll
+	      ticking = true;
+	      if (currentSlideNumber !== 0) {
+	        currentSlideNumber--;
+	      }
+	      previousItem();
+	      slideDurationTimeout(slideDurationSetting);
+	    }
+	  }
+	}
+
+	// ------------- SET TIMEOUT TO TEMPORARILY "LOCK" SLIDES ------------- //
+	function slideDurationTimeout(slideDuration) {
+	  setTimeout(function() {
+	    ticking = false;
+	  }, slideDuration);
+	}
+
+	// ------------- ADD EVENT LISTENER ------------- //
+	var mousewheelEvent = isFirefox ? "DOMMouseScroll" : "wheel";
+	window.addEventListener(mousewheelEvent, _.throttle(parallaxScroll, 60), false);
+
+	// ------------- SLIDE MOTION ------------- //
+	function nextItem() {
+	  var $previousSlide = $(".background").eq(currentSlideNumber - 1);
+	  $previousSlide.css("transform", "translate3d(0,-130vh,0)").find(".content-wrapper").css("transform", "translateY(40vh)");
+	  currentSlideTransition();
+	}
+
+	function previousItem() {
+	  var $previousSlide = $(".background").eq(currentSlideNumber + 1);
+	  $previousSlide.css("transform", "translate3d(0,30vh,0)").find(".content-wrapper").css("transform", "translateY(30vh)");
+	  currentSlideTransition();
+	}
+
+	function currentSlideTransition() {
+	  var $currentSlide = $(".background").eq(currentSlideNumber);
+	  $currentSlide.css("transform", "translate3d(0,-15vh,0)").find(".content-wrapper").css("transform", "translateY(15vh)");;
+	}
+
 });
-})
